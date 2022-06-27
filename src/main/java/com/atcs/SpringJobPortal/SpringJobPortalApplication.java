9e15dcecd7f028e82ffb74ae95d601a0681b8534
sidhu8300 <jainsiddarth83@gmail.com>
2022-06-27 13:11:57 +0530

    intial by siddarth

---
+++ src/main/java/com/atcs/SpringJobPortal/SpringJobPortalApplication.java
@@ -0,0 +1,13 @@
+package com.atcs.SpringJobPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJobPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJobPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atcs/SpringJobPortal/SpringJobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atcs.SpringJobPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
